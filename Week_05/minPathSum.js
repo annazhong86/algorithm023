737cc3aca2681e6bc6f05f83bdaa3780bf799e5c
annazhong86 <xiaomeimengyu@126.com>
2021-01-31 23:04:08 +0800

    added week05 homework

---
+++ Week_05/minPathSum.js
@@ -0,0 +1,22 @@
+var minPathSum = function(grid) {
+    let m = grid.length;
+    let n = grid[0].length;
+    let dp = new Array(m).fill(0).map(() => new Array(n).fill(0));
+    dp[0][0] = grid[0][0];
+    for(let i = 0; i < m; i++){
+        for(let j = 0; j< n; j++){
+            if(i > 0 && j == 0){
+                dp[i][0] = dp[i-1][0] + grid[i][0];
+            }
+            if(i == 0 && j > 0){
+                dp[0][j] = dp[0][j-1] + grid[0][j]
+            }
+
+            if(i > 0 && j > 0){
+                dp[i][j] = Math.min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+            }
+        }
+    }
+
+    return dp[m-1][n-1];
+};
---
+++ Week_05/递归代码模板.js
@@ -0,0 +1,15 @@
+//JavaScript
+const recursion = (level, params) => {
+    //recursion terminator
+    if(leevel > MAX_LEVEL){
+        process_result
+        return
+    }
+
+    //process current level
+    process(level, params)
+    //drill down
+    recursion(level + 1, params)
+    //clean current level status if needed
+    
+}
